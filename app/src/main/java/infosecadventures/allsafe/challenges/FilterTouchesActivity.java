21f48a41207f8b68b0a89a9d78c84d030ebd1426
louis <tenglouistjj@gmail.com>
2026-01-29 16:00:48 +0800

    Fix bugs

--- app/src/main/java/infosecadventures/allsafe/challenges/FilterTouchesActivity.java
+++ app/src/main/java/infosecadventures/allsafe/challenges/FilterTouchesActivity.java
@@ -2,6 +2,7 @@ package infosecadventures.allsafe.challenges;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,19 +15,17 @@ public class FilterTouchesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_touches);
 
-        // Vulnerability: Setting this to false allows touches to be processed
-        // even when the window is obscured by other windows or dialogs.
-        // This can allow attackers to intercept or simulate touches.
-        getWindow().setFilterTouchesWhenObscured(false);
+        // Apply to the root view of the activity
+        View rootView = findViewById(android.R.id.content);
+        rootView.setFilterTouchesWhenObscured(false);
 
         TextView explanationText = findViewById(R.id.filter_touches_explanation);
         if (explanationText != null) {
-            explanationText.setText("This activity demonstrates the vulnerability of setFilterTouchesWhenObscured(false).\n\n" +
-                    "When this flag is set to false, touches are processed even if the window is obscured " +
-                    "by other windows or dialogs. This can allow attackers to:\n" +
-                    "- Intercept user touches intended for other applications\n" +
-                    "- Simulate touches without the user's knowledge\n" +
-                    "- Perform unauthorized actions on behalf of the user");
+            explanationText.setText(
+                    "This activity demonstrates an insecure configuration of filterTouchesWhenObscured.\n\n" +
+                            "When set to false on a View, touches are accepted even if the window is obscured, " +
+                            "allowing tapjacking-style attacks."
+            );
         }
     }
 }
